e1c68c1eedac39ed1b01997fd01c09d4f8cdc9e9
Brian Schlenker <bschlenk@umich.edu>
2014-03-31 23:41:23 -0400

    added GSSMechList class - wraps gss_OID_set

--- include/gsspp/gssmech.h
+++ include/gsspp/gssmech.h
@@ -3,6 +3,7 @@
 
 #include <gssapi.h>
 #include <string>
+#include <vector>
 
 class GSSMech
 {
@@ -11,11 +12,30 @@ class GSSMech
 	GSSMech( gss_OID_desc_struct * oid ) : _oid( oid ) {}
 	GSSMech( const std::string& mech_name );
 
-	operator gss_OID  () { return _oid; }
+	operator gss_OID  () { return _oid;   }
 	operator gss_OID *() { return &_oid;  }
 
  private:
 	gss_OID _oid;
+};
+
+class GSSMechList
+{
+ public:
+	GSSMechList()  { inquire(); }
+	~GSSMechList() { clear();   }
+
+	void     inquire();
+	void     clear();
+	size_t   size() { return _oid_set->count; }
+	gss_OID operator[]( size_t index );
+	gss_OID at( size_t index );
+
+	operator gss_OID_set  () { return _oid_set;  }
+	operator gss_OID_set *() { return &_oid_set; }
+
+ private:
+	gss_OID_set _oid_set;
 };
 
 #endif // __GSSMECH_H__
--- src/gssmech.cpp
+++ src/gssmech.cpp
@@ -4,6 +4,7 @@
 #include <gssapi.h>
 #include <string>
 #include <algorithm>
+#include <stdexcept>
 using std::string;
 using std::transform;
 
@@ -15,4 +16,32 @@ GSSMech::GSSMech( const string& mech_name )
 
 	if ( maj != GSS_S_COMPLETE )
 		throw GSSException( maj, min, "gss_str_to_oid" );
+}
+
+gss_OID GSSMechList::operator[]( size_t index )
+{
+	return ( _oid_set->elements + index );
+}
+
+gss_OID GSSMechList::at( size_t index )
+{
+	if ( index >= _oid_set->count )
+		throw std::out_of_range( "index out of range" );
+
+	return ( *this )[ index ];
+}
+
+void GSSMechList::inquire()
+{
+	OM_uint32 maj, min;
+	maj = gss_indicate_mechs( &min, &_oid_set );
+
+	if ( maj != GSS_S_COMPLETE )
+		throw GSSException( maj, min, "gss_indicate_mechs" );
+}
+
+void GSSMechList::clear()
+{
+	OM_uint32 maj, min;
+	gss_release_oid_set( &min, &_oid_set );
 }
